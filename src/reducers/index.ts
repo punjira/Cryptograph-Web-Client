3af961789288aef3619ac2fc93b7c011ee01793b
punjira <saeedwmail@gmail.com>
2021-10-22 14:06:46 +0330

    combine market sentiment reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,21 @@
+/**
+ * root reducer.
+ *
+ * all reducers are put here in the first level and no other combine reducer \
+ * is defined inside application.
+ *
+ * @todo
+ * redesign combine reducer and create a custom one
+ */
+
 import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({});
+import marketStateReducer from "./reducers/market-sentiment.reducer";
 
-export type RootReducer = ReturnType<typeof rootReducer>;
+const rootReducer = combineReducers({
+  marketStateReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
