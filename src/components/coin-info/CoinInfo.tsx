d0544f63678d78da24791aed82ba9842b8eae968
punjira <saeedwmail@gmail.com>
2021-10-28 21:07:39 +0330

    add coin info component

---
+++ src/components/coin-info/CoinInfo.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducers";
+import { selectPrice } from "../../selectors/latest-price.selector";
+import { Coin } from "../../types/market.types";
+
+interface CoinInfoProps extends Coin {}
+
+const CoinInfo: FC<CoinInfoProps> = ({ ...props }) => {
+  const latest_price = useSelector((state: RootState) =>
+    selectPrice(state, props.ticker)
+  );
+  return (
+    <div className="flex flex-row">
+      <img src={props.image} className="w-16 h-16" />
+      <div className="flex flex-col ml-1">
+        <h1 className="font-bold">{props.name}</h1>
+        <h3 className="text-sm">{props.ticker}</h3>
+        <span>${latest_price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CoinInfo;
