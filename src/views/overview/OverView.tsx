d34d0ecf851fd94e89ba8049f0c4df95a380fa5b
punjira <saeedwmail@gmail.com>
2021-10-28 20:37:45 +0330

    fetch last prices on load

--- src/views/overview/OverView.tsx
+++ src/views/overview/OverView.tsx
@@ -1,15 +1,27 @@
-import { FC } from "react";
-import { useAppSelector } from "../../hooks/typed-selector";
+import { FC, useEffect } from "react";
+import { getLatestPriceActionCreator } from "../../actions/latest-price.action";
+import CoinInfo from "../../components/coin-info/CoinInfo";
+import { useAppDispatch, useAppSelector } from "../../hooks/typed-selector";
 import PageLayout from "../../layout/PageLayout";
 import MarketBar from "./components/MarketBar";
+import useActiveHook from "./hooks/useActiveHook";
 
 const OverView: FC = () => {
   const trendingMarkets = useAppSelector(
     state => state.tendingMarketsReducer.data
   );
+  const [active, handleClick, clear] = useActiveHook(trendingMarkets);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getLatestPriceActionCreator());
+    return () => {
+      clear();
+    };
+  }, []);
   return (
     <PageLayout>
       <MarketBar markets={trendingMarkets} />
+      <div>{active && <CoinInfo {...active} />}</div>
     </PageLayout>
   );
 };
