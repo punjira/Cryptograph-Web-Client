d148d3a574c1db0143a8edbf283a5cd9b6fef96e
punjira <saeedwmail@gmail.com>
2021-10-28 12:31:12 +0330

    add menu

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import Item, { MenuItemProps } from "./components/Item";
+import {
+  FaHome,
+  FaChartPie,
+  FaUserAlt,
+  FaHeart,
+  FaReadme
+} from "react-icons/fa";
+
+const icon_default_style = "text-md text-gray-300 m-1";
+
+const options: MenuItemProps[] = [
+  {
+    icon: <FaHome title="home" className={icon_default_style} />,
+    link: "/app"
+  },
+  {
+    icon: <FaChartPie title="markets" className={icon_default_style} />,
+    link: "/market"
+  },
+  {
+    icon: <FaUserAlt title="user" className={icon_default_style} />,
+    link: "/user"
+  },
+  {
+    icon: <FaReadme title="blog" className={icon_default_style} />,
+    link: "/blog"
+  },
+  {
+    icon: <FaHeart title="support" className={icon_default_style} />,
+    link: "/support"
+  }
+];
+
+const SideMenu: FC = props => {
+  return (
+    <div className="fixed flex flex-col align-middle bottom-2">
+      {options.map((el, index) => (
+        <Item {...el} animationOrder={options.length - index} key={el.link} />
+      ))}
+    </div>
+  );
+};
+
+export default SideMenu;
