3786ba7007aa1e89ce6d071d00b07ae28ec05d2d
punjira <saeedwmail@gmail.com>
2021-10-28 10:21:01 +0330

    add market sentiment bar

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import MarketSentiment from "../../components/market-sentiment-bar/MarketSentimentBar";
 
 const App: FC = () => {
-  return <div>hi</div>;
+  return (
+    <div className="font-sans font-thin">
+      <MarketSentiment />
+    </div>
+  );
 };
 
 export default App;
