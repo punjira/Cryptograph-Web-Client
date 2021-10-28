078b9aba92875167ec3680d9e7b037ebf1474730
punjira <saeedwmail@gmail.com>
2021-10-28 14:25:15 +0330

    add router

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import MarketSentiment from "../../components/market-sentiment-bar/MarketSentimentBar";
 import Menu from "../../components/menu/Menu";
+import OverView from "../../views/overview/OverView";
 
 const App: FC = () => {
   return (
     <div className="font-sans font-thin">
       <MarketSentiment />
       <Menu />
+      <Switch>
+        <Redirect exact from="/" to="/overview" />
+        <Route path="/overview" component={OverView} />
+      </Switch>
     </div>
   );
 };
