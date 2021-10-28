7073244a9041249a9316fc3bcda7b671fc97f74c
punjira <saeedwmail@gmail.com>
2021-10-28 10:19:44 +0330

    add market sentiment hook

--- src/hooks/use-market-sentiment.ts
+++ src/hooks/use-market-sentiment.ts
@@ -1,6 +1,6 @@
 import { useAppSelector } from "./typed-selector";
 
-export function useMarketSentiment() {
+export function useMarketSentiment(): [number | undefined, boolean] {
   const sentiment = useAppSelector(state => state.marketStateReducer.sentiment);
   const isFetching = useAppSelector(
     state => state.marketStateReducer.isFetching
